0dd3227b1f4d8a7222650c9f653e60d1258a33d0
BrunoDoCarmo <bruno_stcarmo3@outlook.com>
2024-12-09 17:59:47 -0300

    Em desenvolvimento

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import { LinkButton } from '../layout/LinkButton'
+
 export function Home() {
     return(
-        <p>Home</p>
+        <section className={styles.home_container}>
+            <h1>Bem vindo ao <span>Coins</span></h1>
+            <p>Começe a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Cria Projeto"/>
+            <img src={savings} alt="Coins"/>
+        </section>
     )
 }
--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,5 +1,33 @@
+import { useHistory } from 'react-router-dom'
+
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
+
 export function NewProject() {
+    const history = useHistory()
+    function createPost(project) {
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/project', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project)
+        })
+            .then((resp) => resp.json())
+            .then((data) =>{
+                history.push('/projects', {message: 'Projeto Criando com Sucesso!'})
+            })
+    }
+
     return(
-        <p>NewProject</p>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie um projeto para depois adicionar os serviços</p>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+        </div>
     )
 }
